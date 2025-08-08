c7b3bdf1a43d0158c1932aa822e18beab9e8bb6e
freeh0b0 <amanindia004@gmail.com>
2025-08-08 18:22:13 +0000

    add proper report functionality

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from stats import *
 
 def main():
-    file_text = get_book_text("./books/frankenstein.txt")
-    num_words = get_num_words(file_text)
-    print(f"{num_words} words found in the document")
+    file_path = "books/frankenstein.txt"
+    file_text = get_book_text(file_path)
+    word_count = get_num_words(file_text)
     char_count = get_char_count(file_text)
-    print(char_count)
+    sorted_count = get_sorted_count(char_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for c in sorted_count:
+        if c["char"].isalpha():     #c is a dictionary with keys "char" and "num"
+            char = c["char"]
+            num = c["num"]
+            print(f"{char}: {num}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,17 @@ def get_char_count(file_text):
             char_count[char] = 0
         char_count[char] += 1
     return char_count       
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_count(char_count):
+    chars = []
+    for c in char_count:
+        item = {}
+        item["char"] = c
+        item["num"] = char_count[c]
+        chars.append(item)
+    chars.sort(reverse=True, key=sort_on)
+
+    return chars
